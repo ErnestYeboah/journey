d4109476373e9488c5a23fb4b4897a601635ff0c
Ernest Yeboah <yeboahernest246@gmail.com>
2025-10-06 11:30:49 +0000

    updated the resumes and added screenshots view

---
+++ js/app.js
@@ -0,0 +1,34 @@
+function openDetailedCardModal(el) {
+  const casestudycardEl = document.querySelector(`.${el}`);
+  casestudycardEl.setAttribute("data-state", "on");
+}
+
+function closeDetailedCardModal(el) {
+  const casestudycardEl = document.querySelector(`.${el}`);
+  casestudycardEl.setAttribute("data-state", "off");
+}
+
+let currentIndex = 0;
+
+function checkSlideContainerState(el) {
+  const figureselides = document.querySelectorAll(`${el} > * `);
+  if (currentIndex <= 0) {
+    currentIndex = 0;
+  } else if (currentIndex > figureselides.length - 1) {
+    currentIndex = figureselides.length - 1;
+  }
+}
+
+function slideLeft(el) {
+  currentIndex--;
+  checkSlideContainerState(el);
+  const figure = document.querySelector(`${el}`);
+  figure.style.translate = `-${currentIndex}00% 0`;
+}
+
+function slideRight(el) {
+  currentIndex++;
+  checkSlideContainerState(el);
+  const figure = document.querySelector(`${el}`);
+  figure.style.translate = `-${currentIndex}00% 0`;
+}
