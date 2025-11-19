4e3cd86d6504b5eb815dd4d27677d621ed1fe22b
Ernest Yeboah <yeboahernest246@gmail.com>
2025-11-19 23:42:30 +0000

    somes changes applied

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
+let currentIndex = 0;
 function openDetailedCardModal(el) {
   const casestudycardEl = document.querySelector(`.${el}`);
   casestudycardEl.setAttribute("data-state", "on");
 }
 
 function closeDetailedCardModal(el) {
+  currentIndex = 0;
   const casestudycardEl = document.querySelector(`.${el}`);
   casestudycardEl.setAttribute("data-state", "off");
 }
-
-let currentIndex = 0;
 
 function checkSlideContainerState(el) {
   const figureselides = document.querySelectorAll(`${el} > * `);
   if (currentIndex <= 0) {
     currentIndex = 0;
-  } else if (currentIndex > figureselides.length - 1) {
+  } else if (currentIndex >= figureselides.length - 1) {
     currentIndex = figureselides.length - 1;
   }
 }
